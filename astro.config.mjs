9dabfacfc4232e94322e4a561590ba0d23c4e891
Antonio <flutotal@gmail.com>
2026-05-11 10:55:30 -0300

    Sincroniza repositorio com siteTesteAntonio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || 'https://flutotal.github.io/',
+	site: process.env.SITE_URL || 'https://example.com',
 	base: process.env.BASE_PATH || '/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog de Gameplays';
+export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Gameplays, reviews rápidas e desafios retro.';
