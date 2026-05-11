334cc5abab953c3e426809fa18dd5a78b22c0b80
Antonio <flutotal@gmail.com>
2026-05-11 09:48:49 -0300

    Atualiza site com conteudo local

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || 'https://example.com',
+	site: process.env.SITE_URL || 'https://flutotal.github.io/',
 	base: process.env.BASE_PATH || '/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
